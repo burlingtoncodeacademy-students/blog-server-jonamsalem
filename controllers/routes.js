f23ab87ae793f48cf7a22cb61caa653d3f0c4968
Jonathan Amsalem <jonathan.amsalem@gmail.com>
2023-06-24 22:24:34 -0400

    added create comment

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,21 +1,21 @@
 let router = require ("express").Router()
 let dbPath = "./api/blog.json"
-let {readData} = require("../helpers/helper-functions")
+let {readData, saveData} = require("../helpers/helper-functions")
 let db = readData(dbPath)
 
 router.get("/all", (req,res) =>{
     try {
         if (db.length == 0){
-            res.status(200).json({
-                message: `The blog is empty`
-        })}
-        else {
+         throw  Error ("There are no blog comments")
+        }  
         res.status(200).json({
             message : db
-        })}
+        })
     }
     catch(err){
-        console.log(err)
+        res.status(400).json({
+            message: `${err}`
+        })
     }
 })
 
@@ -31,8 +31,26 @@ router.get("/:id", (req,res)=> {
         })
     }
     catch (err) {
-        res.status(500).json({
-            message:  ` ID not found`
+        res.status(404).json({
+            message: `${err}`
+        })
+    }
+})
+
+
+router.post("/create", (req,res) =>{
+    try{
+        if (Object.keys(req.body).length <4) throw new Error("Please fill all the requirements")
+        let newPost = {...req.body}
+        db.push(newPost)
+        saveData(dbPath, db)
+        res.status(200).json({
+            message: `blog activity added `
+        })
+    }
+    catch (err){
+        res.status(404).json({
+            message : `${err}`
         })
     }
 })
--- helpers/helper-functions.js
+++ helpers/helper-functions.js
@@ -2,8 +2,19 @@ let fs = require("fs")
 
 function readData(dbPath){
     let file = fs.readFileSync(dbPath)
+    if (!file.length){
+        return []
+    }
+    else{
     return JSON.parse(file)
+    }
 }
 
 
-module.exports = {readData}
+function saveData(dbPath, data){
+    fs.writeFileSync(dbPath, JSON.stringify(data))
+
+}
+
+
+module.exports = {readData, saveData}
